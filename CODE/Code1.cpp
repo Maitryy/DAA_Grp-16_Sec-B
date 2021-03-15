6eb8f7ccae985eb809a16b42d58faf0303f62abd
Aryan <infoaryangupta1809@gmail.com>
2021-03-16 00:57:17 +0530

    Code added

---
+++ CODE/Code1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binsearch(int a[],int b[],int n)
+{
+    int start=0,end=n-1;
+    int mid,res;
+    while(start<=end)
+    {
+        mid= start+((end-start)/2);
+        if(a[mid]==b[mid])
+        {
+            start=mid+1;;
+        }
+        else if(a[mid]!=b[mid])
+        {
+        	res=mid;
+        	end=mid-1;
+        }
+    }
+     return b[res]; 
+}
+
+int main() 
+{
+	int n;
+	cin>>n;
+	int a[n],b[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	b[0]=a[0];
+	int d=(a[n-1]-a[0])/n;
+	for(int i=1;i<n;i++)
+	{
+		b[i]=b[i-1]+d;
+	}
+	int ans=binsearch(a,b,n);
+	cout<<"The missing number is: "<<ans<<endl;
+	return 0;
+}
